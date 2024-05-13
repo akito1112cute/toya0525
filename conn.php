c538e45bf3d99b574ff7d5ed6d72afb4f9be531d
akito1112cute <chi120539@gmail.com>
2024-05-14 07:34:54 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-// gist:45ec057b00e41194079f6472a3f76c78
+// 45ec057b00e41194079f6472a3f76c78
 $link = new mysqli('localhost','NAME','PASSWORD','kano');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {

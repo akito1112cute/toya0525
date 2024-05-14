81b55371cda24e670a984762d5e9440a766e5fa8
akito1112cute <chi120539@gmail.com>
2024-05-14 08:09:04 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,5 @@
 <?php
-// CREATE DATABASE kano DEFAULT CHARSET=utf8;
-USE kano;
-SOURCE kano.sql;
+// 
 $link = new mysqli('localhost','NAME','PASSWORD','kano');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {

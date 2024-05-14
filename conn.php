a2305d6b25fc53b940a90ac4b1e8a4e1596c8371
akito1112cute <chi120539@gmail.com>
2024-05-14 08:01:06 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,1 @@
-<?php
-// 
-$link = new mysqli('localhost','NAME','PASSWORD','kano');
-mysqli_set_charset($link, 'utf8');
-if ($link->connect_error) {
-    die("Failed to connect: " . $conn->connect_error);
-}
-$ranking = "kano_rank";
+
